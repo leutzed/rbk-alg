5b8fea40863beeaa9f53d148c4f8d8dd0f2137bc
Daniel Leutze <leutzed@gmail.com>
2023-12-08 14:57:10 +0000

    levs

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,18 +3,39 @@ fetch("oll.json")
   .then((data) => {
     console.log(data);
     const oll = data.olls;
-    const mappedOll = oll.map((item, index) => {
-      console.log(`${item.index}`);
-      console.log(`${index + 1}`);
-      return `
-        <div class="content">
-          <div>${item.index}</div>
-          <div class="image-block">
-            <img style="height: 4rem; width: 4rem;" src="${item.img}" alt="${item.name}">
+    const categorizedOll = oll.reduce((acc, item) => {
+      if (!acc[item.category]) {
+        acc[item.category] = [];
+      }
+      acc[item.category].push(item);
+      return acc;
+    }, {});
+
+    const mappedOll = Object.entries(categorizedOll).map(([category, items]) => {
+      const sortedItems = items.sort((a, b) => a.id - b.id); // Sort items by id
+      const mappedItems = sortedItems.map((item, index) => {
+        console.log(`${item.index}`);
+        console.log(`${index + 1}`);
+        return `
+          <div class="content align">
+            <div class="flex justify">${item.index}</div>
+            <div class="image-block flex justify">
+              <img style="height: 4rem; width: 4rem;" src="${item.img}" alt="${item.name}">
+            </div>
+            <div class="alg flex justify">${item.alg}</div>
           </div>
-          <div class="alg">${item.alg}</div>
+        `;
+      });
+      return `
+        <div class="category">
+          <h2 class="flex justify">${category}</h2>
+          ${mappedItems.join("")}
         </div>
       `;
     });
+
     document.querySelector("#olls").innerHTML = mappedOll.join("");
   });
+
+
+  
