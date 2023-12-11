2c02e0651fc3e260250bdcaa8741ef816f19305d
Daniel Leutze <leutzed@gmail.com>
2023-12-11 19:04:52 +0000

    added some images

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,7 +3,11 @@ fetch("oll.json")
   .then((data) => {
     console.log(data);
     const oll = data.olls;
-    const categorizedOll = oll.reduce((acc, item) => {
+
+    // order by oll.order
+    const sortedOll = oll.sort((a, b) => a.order - b.order);
+
+    const categorizedOll = sortedOll.reduce((acc, item) => {
       if (!acc[item.category]) {
         acc[item.category] = [];
       }
@@ -11,31 +15,37 @@ fetch("oll.json")
       return acc;
     }, {});
 
-    const mappedOll = Object.entries(categorizedOll).map(([category, items]) => {
-      const sortedItems = items.sort((a, b) => a.id - b.id); // Sort items by id
-      const mappedItems = sortedItems.map((item, index) => {
-        console.log(`${item.index}`);
-        console.log(`${index + 1}`);
-        return `
+    const mappedOll = Object.entries(categorizedOll).map(
+      ([category, items]) => {
+        const sortedItems = items.sort((a, b) => a.id - b.id); // Sort items by id
+        const mappedItems = sortedItems.map((item, index) => {
+          console.log(`${item.index}`);
+          console.log(`${index + 1}`);
+
+          return `
           <div class="content align">
-            <div class="flex justify">${item.index}</div>
-            <div class="image-block flex justify">
-              <img style="height: 4rem; width: 4rem; rotate: ${item.rotate}deg;" src="${item.img}" alt="${item.name}">
+            <div id="number-name" class="flex justify align column">
+              <p class="number">${item.index}</p>
+              <p class="name">${item.name}</p>
             </div>
-            <div class="alg flex justify">${item.alg}</div>
+            <div class="image-block flex justify">
+              <img class="img-case" style="rotate: ${item.rotate}deg;" src="${item.img}" alt="${item.name}">
+            </div>
+            <div class="flex justify align column">
+              <p class="alg">${item.alg}</p>
+              <p class="altAlg">${item.alternativeAlg}</p>
+            </div>
           </div>
         `;
-      });
-      return `
+        });
+        return `
         <div class="category">
           <h2 class="flex justify">${category}</h2>
           ${mappedItems.join("")}
         </div>
       `;
-    });
+      }
+    );
 
     document.querySelector("#olls").innerHTML = mappedOll.join("");
   });
-
-
-  
