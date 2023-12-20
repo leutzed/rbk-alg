8d18fce7ba29540ab7cae91fdcb4de4db1fb6d03
Daniel Leutze <leutzed@gmail.com>
2023-12-20 14:26:12 +0000

    added pll and stylized new things

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,4 @@
-fetch("oll.json")
+fetch("../../oll.json")
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
@@ -48,4 +48,56 @@ fetch("oll.json")
     );
 
     document.querySelector("#olls").innerHTML = mappedOll.join("");
+  });
+
+  fetch("../../pll.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    const pll = data.plls;
+
+    // order by pll.order
+    const sortedPll = pll.sort((a, b) => a.order - b.order);
+
+    const categorizedPll = sortedPll.reduce((acc, item) => {
+      if (!acc[item.category]) {
+        acc[item.category] = [];
+      }
+      acc[item.category].push(item);
+      return acc;
+    }, {});
+
+    const mappedPll = Object.entries(categorizedPll).map(
+      ([category, items]) => {
+        const sortedItems = items.sort((a, b) => a.id - b.id); // Sort items by id
+        const mappedItems = sortedItems.map((item, index) => {
+          console.log(`${item.index}`);
+          console.log(`${index + 1}`);
+
+          return `
+          <div class="content align">
+            <div id="number-name" class="flex justify align column">
+              <p class="number">${item.index}</p>
+              <p class="name">${item.name}</p>
+            </div>
+            <div class="image-block flex justify">
+              <img class="img-case" style="rotate: ${item.rotate}deg;" src="${item.img}" alt="${item.name}">
+            </div>
+            <div class="flex justify align column">
+              <p class="alg">${item.alg}</p>
+              <p class="altAlg">${item.alternativeAlg}</p>
+            </div>
+          </div>
+        `;
+        });
+        return `
+        <div class="category">
+          <h2 class="flex justify">${category}</h2>
+          ${mappedItems.join("")}
+        </div>
+      `;
+      }
+    );
+
+    document.querySelector("#plls").innerHTML = mappedPll.join("");
   });
